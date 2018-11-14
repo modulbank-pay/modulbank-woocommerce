59d2ee60e38d350387a9a6f99678ea4ac7a6bb62
Yuiry Vasilyev <yuriy@y-vasilyev.ru>
2018-11-14 17:08:42 +0700

    убрал тестовый URL

--- modulbank/inc/fpayments_config.php
+++ modulbank/inc/fpayments_config.php
@@ -1,8 +1,7 @@
 <?php
 
 class FPaymentsConfig {
-    const HOST = 'http://172.21.11.3';
-    //const HOST = 'https://pay.modulbank.ru';
+    const HOST = 'https://pay.modulbank.ru';
     const NAME = 'Modulbank';
     const PREFIX = 'modulbank';
 }
